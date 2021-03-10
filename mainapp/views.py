41d6dafa0e215c3ebea4af73ba042f89566f465b
SDubovskoy <tptpee@gmail.com>
2021-03-10 11:31:17 +0300

    dz3

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,32 +1,37 @@
 from django.shortcuts import render
 
-
 # Create your views here.
+from mainapp.models import Product, ProductCategory
+
 
 def main(request):
+    title = 'главная'
+    products = Product.objects.all()[:4]
     content = {
-        'title': 'Главная',
+        'title': title, 'products': products
     }
     return render(request, 'mainapp/index.html', content)
 
 
-def products(request):
-    links_menu = [
-        {'href': 'products_all', 'name': 'все'},
-        {'href': 'products_home', 'name': 'дом'},
-        {'href': 'products_office', 'name': 'офис'},
-        {'href': 'products_modern', 'name': 'модерн'},
-        {'href': 'products_classic', 'name': 'классика'},
-    ]
+def products(request, pk=None):
+    same_products = Product.objects.all()[:4]
+    links_menu = ProductCategory.objects.all()
     content = {
         'title': 'Продукты',
         'links_menu': links_menu,
+        'same_products': same_products,
     }
     return render(request, 'mainapp/products.html', content)
 
 
 def contact(request):
+    contacts = [
+        {'city': 'Moscow', 'phone': '+7 999 888 77 66', 'email': 'info1@geekshop.ru', 'address': 'за МКАД'},
+        {'city': 'Калуга', 'phone': '+7 666 555 44 33', 'email': 'info2@geekshop.ru', 'address': 'МКАД'},
+        {'city': 'Хабаровск', 'phone': '+7 333 222 11 00', 'email': 'info3@geekshop.ru', 'address': 'в пределах МКАД'},
+    ]
     content = {
         'title': 'Контакты',
+        'contacts': contacts,
     }
     return render(request, 'mainapp/contact.html', content)
